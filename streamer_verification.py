f6dea81109662ed84fe1fb354320acdaa1e53c12
Vasanth3g <vasanth3g@gmail.com>
2019-03-19 19:10:09 +0530

    Update streamer_verification.py

--- streamer_verification.py
+++ streamer_verification.py
@@ -3,7 +3,7 @@ pack1=( 'ABC DEF GHI' )
 
 pack_var=maxx_pack1.split(" ")
 
-buck_pack2=('JKL MNO PQR ')
+bu_pack2=('JKL MNO PQR ')
 
 pack2=buck_pack2.split(" ")
 
@@ -15,12 +15,12 @@ ssss_pack1=buck_pack1.split(" ")
 ips={'z.z.z.z w.w.w.w'}
 
 def south_channels():
-    print('checking %d  eeee pack1 channels in maxxsouth streamers' % len(pack_var))
+    print('checking %d  eeee pack1 channels in maxx streamers' % len(pack_var))
     for vvv_channels in pack_var:
         for ip in ips:
             connection = urllib3.PoolManager()
 
-            input=('%s:8006/EvoLive/%s/STB.m3u8' % (ip,vvv_channels))
+            input=('%s:8006/Live/%s/STB.m3u8' % (ip,vvv_channels))
             r=connection.request("GET", input)
             #print(r.status)
             if r.status != 200:
@@ -28,26 +28,26 @@ def south_channels():
 
 
 def eye_channels():
-    print('checking %d  dddd pack1 channels in maxxsouth streamers' % len(pack2))
+    print('checking %d  dddd pack1 channels in maxx streamers' % len(pack2))
     for vvv_channels_1 in pack2:
         for ip in ips:
             connection = urllib3.PoolManager()
-            input=('%s:8006/EvoLive/%s/STB.m3u8' % (ip,pack2))
+            input=('%s:8006/Live/%s/STB.m3u8' % (ip,pack2))
             r=connection.request("GET", input)
             #print(r.status)
             if r.status != 200:
                 print('%s channel was problem not connecting' % vvv_channels_1 )
 
 def uckye_channels2():
-    print('checking %d  cccc pack2 channels in maxxsouth streamers' % len(ssss_pack1))
-    for buck_channels_2 in ssss_pack1:
+    print('checking %d  cccc pack2 channels in maxx streamers' % len(ssss_pack1))
+    for bu_channels_2 in ssss_pack1:
         for ip in ips:
             connection = urllib3.PoolManager()
 
-            input=('%s:8006/EvoLive/%s/STB.m3u8' % (ip,buck_channels_2))
+            input=('%s:8006/Live/%s/STB.m3u8' % (ip,bu_channels_2))
             r=connection.request("GET", input)
             if r.status != 200:
-                print('%s channel was problem not connecting ' % buck_channels_2)
+                print('%s channel was problem not connecting ' % bu_channels_2)
 
 #
 #south_channels()
